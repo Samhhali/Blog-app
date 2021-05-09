fe414429eadf50813308bf4f02bb38259bac6649
samhhali <samahalimuhammad@gmail.com>
2021-05-10 01:11:51 +0200

    Finished Server-side

--- server/routes/index.js
+++ server/routes/index.js
@@ -4,34 +4,34 @@ var Category = require("../models/Category");
 var Post = require("../models/Post");
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
+router.get('/', function (req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
-router.get('/category', (req, res, next)=>{
-  Category.find((err, categories)=> {
-    if(err) return next(err);
+router.get('/category', (req, res, next) => {
+  Category.find((err, categories) => {
+    if (err) return next(err);
     res.json(categories)
   })
 });
 // find post by category id
-router.get('/bycategory/:id', (req, res, next)=>{
-  Post.find({category: req.params.id},(err, posts)=> {
-    if(err) return next(err);
+router.get('/bycategory/:id', (req, res, next) => {
+  Post.find({ category: req.params.id }, (err, posts) => {
+    if (err) return next(err);
     res.json(posts)
   })
 });
 
-router.get('/post', (req, res, next)=>{
-  Post.find((err, posts)=> {
-    if(err) return next(err);
+router.get('/post', (req, res, next) => {
+  Post.find((err, posts) => {
+    if (err) return next(err);
     res.json(posts)
   })
 });
 
-router.get('/post/:id', (req, res, next)=>{
-  Post.findById(req.params.id, (err, posts)=> {
-    if(err) return next(err);
+router.get('/post/:id', (req, res, next) => {
+  Post.findById(req.params.id, (err, posts) => {
+    if (err) return next(err);
     res.json(posts)
   })
 });
