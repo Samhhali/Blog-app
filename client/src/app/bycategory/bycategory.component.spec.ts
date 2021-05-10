e9c9c5c2ab364f98f0f4ac9f438597fff9a3df86
samhhali <samahalimuhammad@gmail.com>
2021-05-10 02:39:37 +0200

    init bycategory component

---
+++ client/src/app/bycategory/bycategory.component.spec.ts
@@ -0,0 +1,25 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { BycategoryComponent } from './bycategory.component';
+
+describe('BycategoryComponent', () => {
+  let component: BycategoryComponent;
+  let fixture: ComponentFixture<BycategoryComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ BycategoryComponent ]
+    })
+    .compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(BycategoryComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
