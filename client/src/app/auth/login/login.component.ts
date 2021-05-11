37159f2ad5b4e869cf418b3528856fed247e2791
samhhali <samahalimuhammad@gmail.com>
2021-05-11 10:13:03 +0200

    Add material login and register Components

--- client/src/app/auth/login/login.component.ts
+++ client/src/app/auth/login/login.component.ts
@@ -1,5 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 
+import {FormBuilder, FormControl, FormGroup, FormGroupDirective, NgForm, Validators} from '@angular/forms';
+import { AuthService } from 'src/app/auth.service';
+import { Router } from '@angular/router';
+import { MyErrorStateMatcher } from '../auth-helper-function';
+
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
@@ -7,9 +12,42 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  // loginForm: FormGroup;
+  // username = '';
+  // password = '';
+  // isLoadingResults = false;
+  // matcher = new MyErrorStateMatcher();
 
-  ngOnInit(): void {
+  loginForm = new FormGroup({});
+  
+  matcher = new MyErrorStateMatcher();
+  isLoadingResults = false;
+
+  constructor(private formBuilder: FormBuilder, private router: Router,
+    private authService: AuthService) { }
+
+    ngOnInit(): void {
+      this.loginForm = this.formBuilder.group({
+        'username' : [null, Validators.required],
+        'password' : [null, Validators.required]
+      });
+    }
+  //function to submit the login form.
+  onFormSubmit(form: NgForm) {
+    this.authService.login(form)
+      .subscribe(res => {
+        console.log(res);
+        if (res.token) {
+          localStorage.setItem('token', res.token);
+          this.router.navigate(['admin']);
+        }
+      }, (err) => {
+        console.log(err);
+      });
   }
-
+  //function to go to the Register page.
+  register() {
+    this.router.navigate(['register']);
+  }
+  
 }
--- client/src/app/auth/register/register.component.ts
+++ client/src/app/auth/register/register.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroupDirective, FormBuilder, FormGroup, NgForm, Validators } from '@angular/forms';
+import { AuthService } from '../../auth.service';
+import { Router } from '@angular/router';
+import { MyErrorStateMatcher } from '../auth-helper-function';
 
 @Component({
   selector: 'app-register',
@@ -7,9 +11,31 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RegisterComponent implements OnInit {
 
-  constructor() { }
+  // registerForm: FormGroup;
+  // fullName = '';
+  // username = '';
+  // password = '';
+  registerForm = new FormGroup({});
+  isLoadingResults = false;
+  matcher = new MyErrorStateMatcher();
+
+  constructor(private formBuilder: FormBuilder,
+    private router: Router, private authService: AuthService) { }
 
   ngOnInit(): void {
+    this.registerForm = this.formBuilder.group({
+      'fullName': [null, Validators.required],
+      'username': [null, Validators.required],
+      'password': [null, Validators.required]
+    });
   }
-
+  onFormSubmit(form: NgForm) {
+    this.authService.register(form)
+      .subscribe(res => {
+        this.router.navigate(['login']);
+      }, (err) => {
+        console.log(err);
+        alert(err.error);
+      });
+  }
 }
