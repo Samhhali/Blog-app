ac1305005abe3aab7446ba9194373dfbcd1fad62
samhhali <samahalimuhammad@gmail.com>
2021-05-11 13:37:36 +0200

    getToken in helper file

---
+++ server/routes/getToken.js
@@ -0,0 +1,13 @@
+//function to get and extract the token from the request headers.
+getToken = function (headers) {
+    if (headers && headers.authorization) {
+        var parted = headers.authorization.split(' ');
+        if (parted.length === 2) {
+            return parted[1];
+        } else {
+            return null;
+        }
+    } else {
+        return null;
+    }
+};
